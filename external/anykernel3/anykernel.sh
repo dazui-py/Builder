c8fd5a1a7ae717b2c81f47d291c6f02813f9e009
dazui-py <dazuii@duck.com>
2026-04-24 22:43:18 +0100

    some minor changes

--- external/anykernel3/anykernel.sh
+++ external/anykernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=rsuntk kernel @ rsuntkOrgs
+kernel.string=dazuii kernel @ dazuii-py
 do.devicecheck=0
 do.modules=0
 do.systemless=0
